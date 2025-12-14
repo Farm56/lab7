e57501a28036f0ef45c94250dd1ff99a22ea8b2f
Farm56 <sstg3455@gmail.com>
2025-12-14 14:52:34 +0700

    Fix output 12.5

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    int a = 20, b = 30;
+    double c = 4.0;       // แก้เป็น double เพื่อให้หารแล้วติดทศนิยม
+    double output;        // ตัวแปรรับผลลัพธ์ต้องเป็น double
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output = a - (b / c); // คำนวณ: 20 - (30/4) = 12.5
+    cout << "output: " << output << "\n"; // แก้คำผิด output และใส่เครื่องหมายคำพูด
     return 0;
 }
